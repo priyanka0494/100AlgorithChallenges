63f7eb0d8106d709b73fa605984a445d04950753
Priyanka Chavan <priyanka.chavan@hm.com>
2024-03-21 15:53:45 +0530

    Add Solution

--- add/add.ts
+++ add/add.ts
@@ -1,10 +1,18 @@
-export function add(param1: number, param2: number): number {
+/**
+    Write a function that returns the sum of two numbers.
 
+    Example
+    For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+ */
+
+export function add(param1: number, param2: number): number {
+    return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-
+    return param1.reduce((a, c) => a + c, 0)
 }
+
 // console.log(add(1, 2));
 // console.log(add(3, 2));
 
